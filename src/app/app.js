cc1c7d2bbc901776b5abab13370919680d44f1a3
Damian <damian.jozefowski@gmail.com>
2015-01-31 12:04:32 +0100

    Dzialavy template projektu

---
+++ src/app/app.js
@@ -0,0 +1,27 @@
+angular.module('myApp',['ui.router'])
+
+
+.config(function($stateProvider) {
+	$stateProvider
+    .state('inbox', {
+      url: "/inbox",
+      controller: 'InboxCtrl',
+      templateUrl: "src/app/views/main.html"
+    })
+    .state('sent', {
+      url: "/sent",
+      templateUrl: "src/app/views/sent.html",
+      controller: 'SentCtrl'
+    })
+    .state('email', {
+      url: "/email/:id",
+      templateUrl: "src/app/views/email.html"
+    });
+})
+
+.controller('AppCtrl', function AppCtrl($scope, $rootScope) {
+
+	$scope.var = 'Hello World';
+})
+
+.run();
